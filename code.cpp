d0134de39b0e785986f038ea2cbabe22c906b57b
SebasAndres <69599597+SebasAndres@users.noreply.github.com>
2022-01-19 18:26:38 -0300

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <deque>
+
+#define forn(max) for (int w=0; w<max; w++)
+
+using namespace std;
+
+void printDeque(deque<int> arreglo) {
+	forn(arreglo.size()) { cout << arreglo.at(w) << " "; }
+	cout << endl;
+}
+
+bool contiene(deque<int> arreglo, int elemento) {
+	forn(arreglo.size()) { if (arreglo.at(w) == elemento) return true; }
+	return false;
+}
+
+int main() {
+
+	int N, Q; cin >> N >> Q;
+
+	deque<int> hilera;
+	deque<int> diferencias;
+	deque<int> distintosMenorK;
+
+	forn(N) { int n; cin >> n; hilera.push_back(n); }
+	forn(Q) {
+		// Movimiento
+		int t, L, R, K;
+		cin >> t >> L >> R >> K;
+
+		L--; R--;
+
+		if (t == 1 && K == 0) {
+			// Evento tipo 1
+			hilera[L] = R;
+		}
+		else if (t == 2) {
+			// Evento tipo 2
+			int max_intervalo = hilera[L]; int min_intervalo = hilera[L];
+			for (int d = L; d <= R; d++) {
+				if (hilera.at(d) > max_intervalo) max_intervalo = hilera.at(d);
+				else if (hilera.at(d) < min_intervalo) min_intervalo = hilera.at(d);
+			}
+			int dif = max_intervalo - min_intervalo;
+			diferencias.push_back(dif);
+
+			int distintosMK_counter = 0;
+			deque<int> elegidos;
+			forn(N) {
+				if (hilera[w] < K && !contiene(elegidos, hilera[w])) {
+					distintosMK_counter++;
+					elegidos.push_back(hilera[w]);
+				}
+			}
+			distintosMenorK.push_back(distintosMK_counter);
+		}
+	}
+
+	printDeque(diferencias);
+	printDeque(distintosMenorK);
+
+	return 0;
+}
+
+
