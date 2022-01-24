cc59841ecb0bc923b8aa54f534d8dea8b9012fd6
Sebastián Andrés <69599597+SebasAndres@users.noreply.github.com>
2022-01-24 14:36:48 -0300

    Delete carteles.cpp

--- carteles.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-using namespace std;
-int main () {
-	string in; cin >> in;
-	cout << string(in.size() + 2, '*') << endl;
-	cout << '*' << in << '*' << endl;
-	cout << string(in.size() + 2, '*') << endl;
-}
