b8a5a81cd65d8dda5c89833b7a1be28875d71f1e
Sebastián Andrés <69599597+SebasAndres@users.noreply.github.com>
2022-01-24 14:43:25 -0300

    Update pintar_pixeles.cpp

--- pintar_pixeles.cpp
+++ pintar_pixeles.cpp
@@ -12,18 +12,12 @@ struct Region {
 
 vector<Region> regiones;
 const int N = 4;
-char C[N][N] = { {'A', 'A', 'R', 'R'},
-				 {'A', 'A', 'A', 'R'},
-				 {'R', 'A', 'V', 'V'},
-				 {'A', 'R', 'V', 'V'} };
+char C[N][N] = { {'A', 'A', 'R', 'R'}, {'A', 'A', 'A', 'R'},
+		 {'R', 'A', 'V', 'V'}, {'A', 'R', 'V', 'V'} };
 
-int f(int x) {
-	if (x < 0) {
-		return 0;
-	}
-	else if (x >= N) {
-		return N-1;
-	}
+int f (int x) {
+   if (x < 0) { return 0; }
+   else if (x >= N) { return N-1; }
 }
 
 void paint(char grid[N][N], vector<Region> regiones, int data[2]) {
@@ -31,7 +25,6 @@ void paint(char grid[N][N], vector<Region> regiones, int data[2]) {
 	int minimo_repeticiones = regiones.size() - 1;
 	cout << "El minimo de repeticiones es: " << minimo_repeticiones << endl;
 	cout << ": ";
-
 	int empty;
 	cin >> empty;
 }
@@ -39,11 +32,7 @@ void paint(char grid[N][N], vector<Region> regiones, int data[2]) {
 bool AllGrid(char color) {
 	bool retorno = true;
 	for (int j = 0; j < N; j++) {
-		for (int i = 0; i < N; i++) {
-			if (C[j][i] != color) {
-				retorno = false;
-			}
-		}
+		for (int i = 0; i < N; i++) { if (C[j][i] != color) { retorno = false; } }
 	}
 	return retorno;
 }
@@ -51,36 +40,22 @@ bool AllGrid(char color) {
 list<char> delete_repeated(list<char> original) {
 	list <char> nlist;
 	for (char c : original) {
-		if (find(original.begin(), original.end(), c) == original.end()) {
-			nlist.push_back(c);
-		}
+		if (find(original.begin(), original.end(), c) == original.end()) { nlist.push_back(c); }
 	}
 	return nlist;
 }
 
 list<char> get_limits(Region r) {
-	list<char> limites;
-	char color = r.color;
+	list<char> limites; char color = r.color;
 	for (int *pixl : r.pixeles) {
-		int j = pixl[0];
-		int i = pixl[1];
-
+		int j = pixl[0]; int i = pixl[1];
 		// Misma fila
-		if (C[f(j + 1)][i] != color) {
-			limites.push_back(C[f(j + 1)][i]);
-		}
-		else if (C[f(j - 1)][i] != color) {
-			limites.push_back(C[f(j - 1)][i]);
-		}
+		if (C[f(j + 1)][i] != color) { limites.push_back(C[f(j + 1)][i]); }
+		else if (C[f(j - 1)][i] != color) { limites.push_back(C[f(j - 1)][i]); }
 
-		if (C[j, f(i + 1)][i] != color) {
-			limites.push_back(C[j][f(i + 1)]);
-		}
-		else if (C[j][f(i - 1)] != color) {
-			limites.push_back(C[j][f(i - 1)]);
-		}
+		if (C[j, f(i + 1)][i] != color) { limites.push_back(C[j][f(i + 1)]); }
+		else if (C[j][f(i - 1)] != color) { limites.push_back(C[j][f(i - 1)]); }
 	};
-
 	limites = delete_repeated(limites);
 	return limites;
 }
@@ -104,51 +79,28 @@ bool inReg(char color, int j, int i) {
 			}
 		}
 	}
-	else {
-		retorno = false;
-	}
+	else { retorno = false; }
 	return retorno;
 }
 
 void pixeles() {
-
 	for (int j = 0; j < N; j++) { // lectura horizontal 
 		for (int i = 0; i < N; i++) {
-
-			if (inReg(C[j][i], j, i)) {// ¿C[j,i] pertenece a una region > 1
-				// la funcion ya añade a C en la region
-			}
-			else {
+			if (!inReg(C[j][i], j, i)) { // Â¿C[j,i] pertenece a una region > 1
 				Region nueva_region;
 				nueva_region.color = C[j][i];
 				nueva_region.pixeles.push_back({ j,i });
 			}
-
 		}
 	}
-	
-	int data[2];
-	
-	int max, contador = 0;
+	int data[2]; int max, contador = 0;
 	int min = regiones[0].pixeles.size();
 	int bigger, smaller;
 	for (Region r: regiones) {
 		r.limites = get_limits(r);
-		if (r.pixeles.size() >= max) {
-			bigger = contador;
-			max = r.pixeles.size();
-		}
-		else if (r.pixeles.size() <= min) {
-			smaller = contador;
-			min = r.pixeles.size();
-		}
-
-		data[0] = bigger;
-		data[1] = smaller;
-
+		if (r.pixeles.size() >= max) { bigger = contador; max = r.pixeles.size(); }
+		else if (r.pixeles.size() <= min) { smaller = contador; min = r.pixeles.size(); }
+		data[0] = bigger; data[1] = smaller;
 	}
-
-	while (AllGrid('A')) {
-		paint(C, regiones, data);
-	}
+	while (AllGrid('A')) { paint(C, regiones, data); }
 }
